a33f83d0a4d08f15ab71a9a5fa31162e5fc9b33e
Anndrei87 <andrei09ka6@gmail.com>
2022-11-16 21:16:15 -0300

    feat: login jwt and logout

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const fs = require('fs');
 const jsonMock = require('./arquivo.json')
+require('dotenv-safe').config({path:'.env'});
 const app = express()
 const jsonParser = bodyParser.json()
+app.use(express.json());
+
+var jwt = require('jsonwebtoken');
 
 // Porta que vai rodar o servidor e uma mensagem para indicar que ligou
 app.listen(3000, () => console.log('servidor rodando na porta 3000'))
@@ -16,24 +20,56 @@ app.post('/register',jsonParser, (req, res) => {
     )
 })
 
+// Retorna todos os usuarios
 app.get('/getRegister', (req, res) => {
     res.status(200).send(
         fs.readFileSync('arquivo.json')
     )
 })
 
+app.post('/login', jsonParser, (req, res, next) => {
+    console.log( "asd")
+    console.log(req.body)
+   
+    if(req.body.nome === 'admin' && req.body.password === 'admin123'){
+      const id = 1;
+      var token = jwt.sign({ id }, process.env.SECRET, {
+        expiresIn: 1000 // expires in 5min
+      });
+      let reqObject =  {
+        "name": req.body.nome,
+        "password": req.body.password,
+        "token":token
+    }
+      res.status(200)
+      res.json({ auth: true, users:[reqObject] });
+     
+    }else{
+        res.status(403)
+        res.json({ erro: "error" });
+    }
+})
+
+app.get('/logout', function(req, res) {
+    res.status(200).send({ auth: false, token: null });
+});
+
+
+  
+
 
 function validateValues(req){
     let jsonLength = Object.keys(jsonMock).length;
     let reqObject =  {
         "name": req.body.name,
-        "password": req.body.password
+        "password": req.body.password,
+        
         }
     if(jsonLength === 0){
         let objeto = {
             users:[reqObject]
         }
-    
+
         return JSON.stringify(objeto)
     } else {
         jsonMock.users.push(reqObject)
